1a9ef21733b888c7860f190e18bced357c026375
wangsdchn <wangsdbit@163.com>
2017-04-12 21:54:19 +0800

    xec

--- laneLine.py
+++ laneLine.py
@@ -11,15 +11,17 @@ def detect(src):
     rows,cols=src.shape[:2]
     print(cols,rows)
     thresh=250
+    roi=gray[rows//2:rows,:]
     kernel=cv2.getStructuringElement(cv2.MORPH_RECT,(1,5),(0,2))
-    while thresh>=50:
-        thresh,binImg=cv2.threshold(gray,thresh,255,cv2.THRESH_BINARY)
-        thresh-=5
+    while thresh>=150:
+        thresh,binImg=cv2.threshold(roi,thresh,255,cv2.THRESH_OTSU)
+        thresh=5
         img0=cv2.erode(binImg,kernel,2)
         binImg=cv2.dilate(img0,kernel,2)
         img,contours,hieracy=cv2.findContours(binImg,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
         size=np.size(contours)
-        for i in range(size):
+        print(size)
+        for i in range(size//4-1):
             rect=cv2.minAreaRect(contours[i])
             box=cv2.boxPoints(rect)
             box=np.int0(box)
@@ -33,7 +35,9 @@ def detect(src):
             if h<10*w and h>0.1*w:
                 continue
             if w*h<200:
-                continue            
+                continue
+            if w*h>cols*rows/20:
+                continue
             [vx,vy,x,y] = cv2.fitLine(contours[i],cv2.DIST_L2,0,0.01,0.01)
             if vx==0:
                 slidRitio=0xFFFF
@@ -41,20 +45,40 @@ def detect(src):
                 slidRitio=vy/vx
             if slidRitio<0.25 and slidRitio>-0.25:
                 continue
-            print(box)
-            lefty = int((-y/slidRitio) + x)
-            righty = int(((rows-y)/slidRitio)+x)
-            print(lefty,righty,x,y)
-            cv2.line(src,(righty,rows),(lefty,0),(0,0,255),2)
-        cv2.imshow('gray',gray)
-        cv2.imshow('src',src)
-        cv2.imshow('binImg',binImg)
-        k=cv2.waitKey(0)&0xFF
-        if k==27:
-            break
+            maxX=0
+            maxY=0
+            maxSlidRitio=0
+            minSlidRitio=9999
+            minX=9999
+            minY=9999
+            if x>cols/2:
+                if minX>x:
+                    minX=x
+                    minSlidRitio=slidRitio
+                    minY=y
+            else:
+                if maxX<x:
+                    maxX=x
+                    maxSlidRitio=slidRitio
+                    maxY=y
+            cv2.imshow('binImg',binImg)
+            k=cv2.waitKey(0)&0xFF
+            if k==27:
+                break
+    #minLefty = int((-minY/minSlidRitio) + minX)
+    #minRighty = int(((rows//2-minY)/minSlidRitio)+minX)
+    #maxLefty = int((-maxY/maxSlidRitio) + maxX)
+    #maxRighty = int(((rows//2-maxY)/maxSlidRitio)+maxX)
+    #cv2.line(src,(minRighty,rows),(minLefty,rows//2),(0,0,255),2)
+    #cv2.line(src,(maxRighty,rows),(maxLefty,rows//2),(0,0,255),2)
+    cv2.imshow('src',src)
+    cv2.imshow('binImg',binImg)
+    k=cv2.waitKey(0)&0xFF
+    #if k==27:
+    #    break
     
 if __name__=='__main__':
-    imgPath='./laneLine.png'
+    imgPath='./13.png'
     src=cv2.imread(imgPath)
     detect(src)
     
